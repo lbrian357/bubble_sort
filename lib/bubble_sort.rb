954fb7119eef480f922f6ba0bb478046d1918fc8
brian <lbrian357@gmail.com>
2016-07-22 07:23:20 +0100

    added block feature

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,15 +1,31 @@
 def bubble_sort(array)
-  until array.min == array[0]
-
+  c = 1
+  until c > array.length-1
     i = 0
     while i < array.length-1
       array[i], array [i+1] = array[i+1], array[i] if array[i]>array[i+1]
       i += 1
     end
-
+    c += 1
   end
   array
-
-
-
 end
+
+def bubble_sort_by(array)
+  c = 1
+  until c > array.length-1
+    i = 0
+    while i < array.length-1
+      result =  yield(array[i],array[i+1])
+      p result
+      if result>=1
+        array[i], array[i+1] = array[i+1], array[i]      
+      end
+      i += 1
+    end
+    c += 1
+  end
+  array
+end
+
+
--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -5,3 +5,15 @@ describe '#bubble_sort' do
     expect(bubble_sort([4,3,78,2,0,2])).to eq([0,2,2,3,4,78])
   end
 end
+
+describe '#bubble_sort_by' do
+  it 'sorts an array of numbers based on a block' do
+    expect(bubble_sort_by([2,5,4,3,2,1]){|left,right| left - right}).to eq([1,2,2,3,4,5])
+  end
+end
+
+describe '#bubble_sort_by' do
+  it 'sorts an array based on a block' do
+    expect(bubble_sort_by(["hi","hello","hey"]){|left, right| left.length-right.length}).to eq(["hi", "hey", "hello"])
+  end
+end
